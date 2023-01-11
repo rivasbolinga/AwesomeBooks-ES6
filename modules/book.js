559b7017da64ca6bdcc9b51659ded66275ad71fb
Andrea <andrearivas4cinfor@gmail.com>
2023-01-11 15:05:05 +0100

    Add modules

---
+++ modules/book.js
@@ -0,0 +1,9 @@
+// class constructor of the book object
+
+export default class Book {
+  constructor(title, author, id) {
+    this.author = author;
+    this.title = title;
+    this.id = id;
+  }
+}
